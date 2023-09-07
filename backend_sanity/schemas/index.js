339656133573d3e3494232d1e7de859417919a97
dabiegorden <117079883+dabiegorden@users.noreply.github.com>
2023-09-06 22:41:58 -0700

    first comit

---
+++ backend_sanity/schemas/index.js
@@ -0,0 +1,20 @@
+// importing schemas
+import abouts from './abouts'
+import brands from './brands'
+import contact from './contact'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import works from './works'
+import testimonials from './testimonials'
+
+export const schemaTypes = [
+  abouts,
+  brands,
+  contact,
+  experiences,
+  skills,
+  workExperience,
+  works,
+  testimonials,
+]
