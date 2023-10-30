66fa97068c202c237417a71b76651beda9dd8fc8
dabiegorden <117079883+dabiegorden@users.noreply.github.com>
2023-10-30 02:29:49 -0700

    update

--- portfolio_frontend/src/components/SocialMedia.jsx
+++ portfolio_frontend/src/components/SocialMedia.jsx
@@ -4,14 +4,20 @@ import { FaFacebookF } from 'react-icons/fa';
 
 const SocialMedia = () => (
   <div className="app__social">
-    <div>
-      <BsTwitter />
+    <div style={{ cursor: "pointer"}}>
+       <a href="https://twitter.com/DABIEGORDEN2">
+        <BsTwitter />
+       </a>
     </div>
-    <div>
-      <FaFacebookF />
+    <div style={{ cursor: "pointer"}}>
+        <a href="GATXUWWYDFFHN4SK64F6H3X6UVUCRGMR6BXJ4JAPT2MMG5QI5VRQLQNE">
+        <FaFacebookF />
+        </a>
     </div>
-    <div>
-      <BsInstagram />
+    <div style={{ cursor: "pointer"}}>
+       <a href="GATXUWWYDFFHN4SK64F6H3X6UVUCRGMR6BXJ4JAPT2MMG5QI5VRQLQNE">
+       <BsInstagram />
+       </a>
     </div>
   </div>
 );
